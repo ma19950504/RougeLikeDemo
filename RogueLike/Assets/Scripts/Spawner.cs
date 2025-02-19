3fc805b1884d56594ee7462427b01c9a5ab787e9
Adam ma <522080658@qq.com>
2025-02-19 20:10:39 +0800

    player 2f范围内不生成enemy

--- RogueLike/Assets/Scripts/Spawner.cs
+++ RogueLike/Assets/Scripts/Spawner.cs
@@ -8,9 +8,11 @@ public class Spawner : MonoBehaviour
     public float timer;
     public float spawnTime;
     public int enemyType;
+    Player player;
 
     void Awake()
     {
+        player = GameManager.instance.player;
         spawnPoints = GetComponentsInChildren<Transform>();  //当前对象及其子对象   在地图选取10个点用于充当生成点
     }
     void Start()
@@ -23,16 +25,26 @@ public class Spawner : MonoBehaviour
         spawnTime = GameManager.instance.enemyPoolManager.prefabs[enemyType].GetComponent<Enemy>().spawnTime;
         timer += Time.deltaTime;
         if (timer > spawnTime)
-        {
-            Spawn();
+        {   
+            Vector3 nextPoint = spawnPoints[Random.Range(1, spawnPoints.Length)].position;
+            float distance = Vector3.Distance(nextPoint, player.transform.position);
+            if (distance <= 2f)
+                return;
+            Spawn(nextPoint);
             timer = 0;
         }
     }
+    void OnDrawGizmos()
+    {
+        if (!Application.isPlaying) return; 
+        Gizmos.color = Color.green; // 设置Gizmo颜色
+        Gizmos.DrawWireSphere(player.transform.position, 2f); // 绘制scanRange的圆
+    }
 
-    public void Spawn()
+    public void Spawn(Vector3 nextPoint)
     {
         GameObject enemy = GameManager.instance.enemyPoolManager.Get(enemyType);
-        enemy.transform.position = spawnPoints[Random.Range(1, spawnPoints.Length)].position;
+        enemy.transform.position = nextPoint;
         GetEnemyType();
     }
     public int GetEnemyType()
